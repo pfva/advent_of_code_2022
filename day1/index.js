4a5468cc72f6ed48b831c7c6e75260beef6fc9f0
Petter <petter.f.andersson@gmail.com>
2022-12-01 07:55:43 +0100

    Add day 1

--- day1/index.js
+++ day1/index.js
@@ -1,7 +1,45 @@
-// const { convertInputToArrayOfNumbers } = require('../utils/index');
+const { convertInputToArrayOfStrings } = require('../utils/index');
 
 /* Part One */
 
-// const input = convertInputToArrayOfNumbers('./input.txt');
+const calories = convertInputToArrayOfStrings('./input.txt');
+
+const calculateCalorieSums = calorieList => {
+  const calorieSums = [];
+  const initialValue = 0;
+
+  calorieList.reduce((total, current) => {
+    if (current === '') {
+      calorieSums.push(total);
+      return initialValue;
+    }
+    return total + parseInt(current);
+  }, initialValue);
+
+  return calorieSums;
+};
+
+const getMaxCaloriesCarried = calories => {
+  const calorieSums = calculateCalorieSums(calories);
+  return Math.max(...calorieSums);
+};
+
+// Answer:
+console.log(getMaxCaloriesCarried(calories));
 
 /* Part Two */
+
+const getTopThreeMaxCalories = calories => {
+  const calorieSums = calculateCalorieSums(calories);
+  const sortedCalorieSums = sortCalorieSums(calorieSums);
+  const topThreeSums = sortedCalorieSums.slice(0, 3);
+
+  return topThreeSums.reduce((total, current) => total + current, 0);
+};
+
+const sortCalorieSums = calorieSums => {
+  return calorieSums.sort((a, b) => a - b).reverse();
+};
+
+// Answer:
+console.log(getTopThreeMaxCalories(calories));
