f607b18c4c8347e21e4d3fad89b8da689d189d75
Petter <petter.f.andersson@gmail.com>
2022-12-03 11:16:36 +0100

    Optimize utility functions

--- utils/index.js
+++ utils/index.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 
 const convertInputToArrayOfNumbers = filePath => {
-  return fs
-    .readFileSync(filePath, 'utf8')
-    .split('\n')
-    .map(number => parseInt(number));
+  return fs.readFileSync(filePath, 'utf8').trimEnd().split('\n').map(Number);
 };
 
 const convertInputToArrayOfStrings = filePath => {
-  return fs.readFileSync(filePath, 'utf8').toString().split('\n');
+  return fs.readFileSync(filePath, 'utf8').trimEnd().toString().split('\n');
 };
 
 const convertInputToArrayOfArrays = filePath => {
   return fs
     .readFileSync(filePath, 'utf8')
+    .trimEnd()
     .toString()
     .split('\n')
     .map(item => item.split(' '));
 };
 
-exports.convertInputToArrayOfNumbers = convertInputToArrayOfNumbers;
-exports.convertInputToArrayOfStrings = convertInputToArrayOfStrings;
-exports.convertInputToArrayOfArrays = convertInputToArrayOfArrays;
+module.exports = {
+  convertInputToArrayOfNumbers,
+  convertInputToArrayOfStrings,
+  convertInputToArrayOfArrays,
+};
